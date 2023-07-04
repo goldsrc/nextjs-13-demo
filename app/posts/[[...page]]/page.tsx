f455c7816bde2357c265c255afff1de83e6775e7
Mohammad Beheshti <mbeheshtis@gmail.com>
2023-07-04 15:23:05 +0200

    better caching for posts

--- app/posts/[[...page]]/page.tsx
+++ app/posts/[[...page]]/page.tsx
@@ -1,6 +1,7 @@
 import Pagination from '@/components/Pagination/Pagination';
 import {prisma} from '@/lib/prisma';
 import Link from 'next/link';
+import {cache} from 'react';
 
 type Props = {
   params: {page: string};
@@ -10,14 +11,20 @@ export default async function Posts({params}: Props) {
   const page = parseInt(params.page, 10) || 1;
   const take = 10;
   const skip = (page - 1) * take;
-  const posts = await prisma.post.findMany({
-    take,
-    skip,
-    include: {
-      author: true,
-    },
+  const getPosts = cache(async (take: number, skip: number) => {
+    const [posts, count] = await Promise.all([
+      prisma.post.findMany({
+        take,
+        skip,
+        include: {
+          author: true,
+        },
+      }),
+      prisma.post.count(),
+    ]);
+    return {posts, count};
   });
-  const count = await prisma.post.count();
+  const {count, posts} = await getPosts(take, skip);
   const totalPages = Math.ceil(count / take);
   return (
     <div>
